746090198b56119f4eba377488fb316f321f8076
Nguyen Tuan <dinhtuan1991vt2013@gmail.com>
2017-10-09 14:26:15 +0700

    Config typescript

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -2,24 +2,15 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import * as React from 'react';
+import { render } from 'react-dom';
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+  render(
     <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
