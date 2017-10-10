dd49bf90ec00b3358be29af8fd459e93b200de52
Nguyen Tuan <dinhtuan1991vt2013@gmail.com>
2017-10-10 09:38:16 +0700

    Config devtools

--- app/javascript/main/store/index.tsx
+++ app/javascript/main/store/index.tsx
@@ -14,11 +14,11 @@ const middleware = [
 ];
 
 if (process.env.NODE_ENV === 'development') {
-  // const devToolsExtension = window.devToolsExtension;
+  const devToolsExtension = window.devToolsExtension;
 
-  // if (typeof devToolsExtension === 'function') {
-  //   enhancers.push(devToolsExtension());
-  // }
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
 }
 
 const composedEnhancers = compose(
